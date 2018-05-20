fb94c7589a90a811eb6f0edc07f0f81ed6bbb7fc
ryotksm <39453788+ryotksm@users.noreply.github.com>
2018-05-21 01:11:28 +0900

    テキストとボタンを追加
    テキストボックスとボタンを追加してボタンのイベントを追加。

---
+++ Assets/GpsPosDataReader.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GpsPosDataReader : MonoBehaviour {
+
+    public InputField fileName;
+
+    // Use this for initialization
+    void Start () {
+        fileName.text = "GPSのPOSファイルをフルパスで指定してください。";
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OnButtonClick()
+    {
+        fileName.text = "GPS!";
+    }
+}
---
+++ Assets/SensorTagDatareader.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+public class SensorTagDatareader : MonoBehaviour {
+
+    public InputField fileName;
+
+	// Use this for initialization
+	void Start () {
+        fileName.text = "センサータグのログファイルをフルパスで指定してください。";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OnButtonClick()
+    {
+        fileName.text = "SensorTag!";
+    }
+
+}
