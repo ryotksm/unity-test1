80e0753156d01f4b25f99f2edb58d6668028ec3f
ryotksm <39453788+ryotksm@users.noreply.github.com>
2018-05-21 01:44:41 +0900

    ファイル読み込み処理を追加

--- Assets/SensorTagDatareader.cs
+++ Assets/SensorTagDatareader.cs
@@ -2,10 +2,13 @@
 using UnityEngine.UI;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
+using System.Text;
 
 public class SensorTagDatareader : MonoBehaviour {
 
     public InputField fileName;
+    public Text textMessage;
 
 	// Use this for initialization
 	void Start () {
@@ -19,7 +22,20 @@ public class SensorTagDatareader : MonoBehaviour {
 
     public void OnButtonClick()
     {
-        fileName.text = "SensorTag!";
-    }
+        string text = "";
+        List<SensorTagData> sensorTagDataList = new List<SensorTagData>();
 
+        using (StreamReader sr = new StreamReader(fileName.text, Encoding.GetEncoding("Shift_JIS")))
+        {
+            // 先頭の2行を読み飛ばす
+            text = sr.ReadLine();
+            text = sr.ReadLine();
+            // データオブジェクト生成
+            while ((text = sr.ReadLine()) != null)
+            {
+                sensorTagDataList.Add(new SensorTagData(text));
+            }
+            textMessage.text = "読み込み完了。" + sensorTagDataList.Count.ToString() + "オブジェクト生成。";
+        }
+    }
 }
